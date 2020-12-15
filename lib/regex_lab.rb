9b7b955f8c0c2f3543348eec2554465825d4692b
Robert Stine <rcorystine@gmail.com>
2020-12-15 01:55:30 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -17,7 +17,7 @@ end
 def first_word_capitalized_and_ends_with_punctuation?(text)
   if text.match(/\w\W/)
     return true
-  elsif(/w[abcdefghijklmnopqrstuvwxyz]\W/)
+  elsif(/\W/)
     return false
   end
 end
