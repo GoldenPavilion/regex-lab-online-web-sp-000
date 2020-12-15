5c5938edb7d0f9581d73e4f440c32837544ed592
Robert Stine <rcorystine@gmail.com>
2020-12-15 02:39:03 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -25,7 +25,7 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-  binding.pry
+  #binding.pry
   if phone.match(/([0-9] *){10}|\(\d{3}\)\d{3}-\d{4}|\(\d{3}\)\d{7}/)
     return true
   end
