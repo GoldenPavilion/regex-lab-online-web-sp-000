2a8dfff5df63320c92f8fe56694b268a17c49dd4
Robert Stine <rcorystine@gmail.com>
2020-12-15 01:20:01 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -7,11 +7,7 @@ def starts_with_a_vowel?(word)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-  if text.grep(/\A+[un]\z+[ing]/)
-    return true
-  else
-    return false
-  end
+  text.grep(/\A+[un]\z+[ing]/)
 end
 
 def words_five_letters_long(text)
